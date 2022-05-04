d8969d3dbbcb82943743dd978fac9a6a01c4e5d8
MandyChadwick <cha33dwick@gmail.com>
2022-05-03 18:25:09 -0800

    this brought up my list of recipes

---
+++ controllers/recipes.js
@@ -0,0 +1,86 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+  const result = await mongodb.getDb().db().collection('recipes').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
+};
+
+const getSingle = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection('recipes')
+    .find({ _id: userId });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
+};
+
+const createRecipe = async (req, res) => {
+  const recipes = {
+    
+    recipeCategory: req.body.recipeCategory,
+    recipeContributer: req.body.recipeContributer,
+    recipeName: req.body.recipeName,
+    recipeURL: req.body.recipeURL
+  };
+  const response = await mongodb.getDb().db().collection('recipes').insertOne(recipes);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the recipe.');
+  }
+};
+
+const updateRecipe = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const recipe = {
+    
+    recipeCategory: req.body.recipeCategory,
+    recipeContributer: req.body.recipeContributer,
+    recipeName: req.body.recipeName,
+    recipeURL: req.body.recipeURL
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('recipes')
+    .replaceOne({ _id: userId }, recipe);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the recipe.');
+  }
+};
+
+const deleteRecipe = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('recipes').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the recipe.');
+  }
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe
+};
+
+
+
+
+
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.use('/recipes', require('./recipes'));
+
+module.exports = router;
